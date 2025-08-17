6edb9671411969eb4b74b44af6075493becf8cbc
mfukechristian <mfukechristian@gmail.com>
2025-08-17 15:08:32 +0200

    git commit -m "Enable Swagger + health endpoints in production"

--- Program.cs
+++ Program.cs
@@ -1,46 +1,33 @@
-using Microsoft.AspNetCore.Mvc;                 
 using Microsoft.EntityFrameworkCore;
 using Sims.API.Data;
-using Sims.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
-        new ObjectResult(new ValidationProblemDetails(context.ModelState))
-        {
-            StatusCode = 405
-        };
-});
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ISimRepository, SimRepository>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+// Apply migrations at startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Enable Swagger always (Prod + Dev)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => Results.Ok(new { status = "up" }));
+app.MapGet("/health", () => "Healthy!");
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
